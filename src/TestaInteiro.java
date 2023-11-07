31400204b50e7a964ba2beec826aad4bb559ad23
Robson-Teixeira <robsonmteixeira@outlook.com>
2023-11-06 22:53:26 -0300

    Tipos e variáveis: int

---
+++ src/TestaInteiro.java
@@ -0,0 +1,21 @@
+
+public class TestaInteiro {
+	public static void main(String[] args) {
+		System.out.println("Olá novo teste");
+
+		int idade = 37;
+		System.out.println("A idade é " + idade);
+
+		idade = 30 + 7;
+		System.out.println("Soma - A idade é " + idade);
+
+		idade = 47 - 10;
+		System.out.println("Subtração - A idade é " + idade);
+
+		idade = (7 * 5) + 2;
+		System.out.println("Multiplicação - A idade é " + idade);
+
+		idade = 74 / 2;
+		System.out.println("Divisão - A idade é " + idade);		
+	}
+}
