7a916b28adb3b019c0dc57d1c9f3b0d2e4fab2eb
Robson-Teixeira <robsonmteixeira@outlook.com>
2023-11-07 00:38:37 -0300

    Praticando condicionais: Testes com switch

---
+++ src/TestaMes.java
@@ -0,0 +1,57 @@
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class TestaMes {
+
+	public static void main(String[] args) {
+
+		Date date = new Date();
+		LocalDate localDate = date.toInstant().atZone(
+				ZoneId.systemDefault()).toLocalDate();
+
+		int mes = localDate.getMonthValue();
+
+		switch (mes) {
+		case 1:
+			System.out.println("O mês é Janeiro");
+			break;
+		case 2:
+			System.out.println("O mês é Fevereiro");
+			break;
+		case 3:
+			System.out.println("O mês é Março");
+			break;
+		case 4:
+			System.out.println("O mês é Abril");
+			break;
+		case 5:
+			System.out.println("O mês é Maio");
+			break;
+		case 6:
+			System.out.println("O mês é Junho");
+			break;
+		case 7:
+			System.out.println("O mês é Julho");
+			break;
+		case 8:
+			System.out.println("O mês é Agosto");
+			break;
+		case 9:
+			System.out.println("O mês é Setembro");
+			break;
+		case 10:
+			System.out.println("O mês é Outubro");
+			break;
+		case 11:
+			System.out.println("O mês é Novembro");
+			break;
+		case 12:
+			System.out.println("O mês é Dezembro");
+			break;
+		default:
+			System.out.println("Mês inválido");
+			break;
+		}
+	}
+}
